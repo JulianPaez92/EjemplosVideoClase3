782c7e5bb864e2774683545500560731d7bdf7a4
JulianPaez92 <jepaez92@gmail.com>
2021-04-28 08:50:44 -0300

    Modificado para tomarlo con Locks

--- globalContador.py
+++ globalContador.py
@@ -2,17 +2,41 @@ import threading
 
 contador = 0
 
+lock = threading.Lock()
+
 def funcion():
     global contador
     for i in range(1000000):
         contador += 1
 
+def hilo1():
+    lock.acquire()
+    try:
+        funcion()
+    finally:
+        lock.release()
+
+def hilo2():
+    lock.acquire()
+    try:
+        funcion()
+    finally:
+        lock.release()
+
+def hilo3():
+    lock.acquire()
+    try:
+        funcion()
+    finally:
+        lock.release()
 print("Inicio programa principal")
 print("Valor Inicial: " + str(contador))
 
-thread_1=threading.Thread(target=funcion)
-thread_2=threading.Thread(target=funcion)
-thread_3=threading.Thread(target=funcion)
+thread_1=threading.Thread(target=hilo1)
+thread_2=threading.Thread(target=hilo2)
+thread_3=threading.Thread(target=hilo3)
+
+
 
 thread_1.start()
 thread_2.start()
